7119cb767c2e43012d5a84b707ae749a4bab0a74
Arif <agafizov@gmail.com>
2019-11-16 12:30:18 +0300

    Play with localStorage variables

--- public/app.js
+++ public/app.js
@@ -1,4 +1,8 @@
-	function something()
-	{
-		alert('Hello everyone!!!');
-	}
+function something()
+{
+	var x = window.localStorage.getItem('aaa');
+
+	window.localStorage.setItem('aaa', 555);
+	
+	alert(x)
+}
