b226c375c9885ba8de3b0a31eb432179384a113f
Arif <agafizov@gmail.com>
2019-11-29 22:35:31 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -25,13 +25,30 @@ function cart_get_number_of_items()
 	var cnt = 0;
 	for(var i = 0; i < window.localStorage.length; i++)
 	{
-		var key = window.localStorage.key(i); // получаем ключ из хеша 
+		var key = window.localStorage.key(i); // получаем ключ из хеша
 		var value = window.localStorage.getItem(key); // получаем значение, аналог ruby hh['key'] = x
 
 		if(key.indexOf('product_') == 0)
 		{
 			cnt = cnt + value * 1;
-		} 
+		}
 	}
 	return cnt;
+}
+
+
+function cart_get_orders()
+{
+	var orders = "";
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ из хеша
+		var value = window.localStorage.getItem(key); // получаем значение, аналог ruby hh['key'] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+	return orders;
 }
