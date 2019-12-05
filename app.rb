8451d83df50fe6437cbf583b1ad50200928750b9
Arif Gafizov <agafizov@gmail.com>
2019-12-05 17:54:46 +0300

    Replace id with product

--- app.rb
+++ app.rb
@@ -23,7 +23,14 @@ end
 
 post '/cart' do
 	orders_input = params[:orders]
-	@orders = parse_orders_input orders_input
+	@items = parse_orders_input orders_input
+
+	@items.each do |item|
+		# id, cnt. id целиком заменяем на весь объект(заказ)
+		item[0] = Product.find(item[0])
+
+	end
+
 	erb :cart
 end
 
