586694303a0d749e7c21308a0ee766790209a970
Arif Gafizov <agafizov@gmail.com>
2019-12-25 15:17:17 +0300

    minor changes

--- public/app.js
+++ public/app.js
@@ -1,14 +1,3 @@
-function something()
-{
-	var x = window.localStorage.getItem('bbb');
-
-	x = x * 1 + 1;
-
-	window.localStorage.setItem('bbb', x);
-
-	alert(x);
-}
-
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
